2f2b8adbac817e8a36ad3b77651d5eaaa15d3fc4
zoo00 <104506641+zoo00@users.noreply.github.com>
2023-06-09 16:25:43 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,71 @@
+import streamlit as st
+
+st.text("hello Streamlit!")
+
+import pytesseract
+from PIL import Image
+import cv2
+import matplotlib.pyplot as plt
+
+
+path = 'C:/Users/wndud/Desktop/nobrand.jpg'
+
+img = Image.open(path)
+st.image(img, width = 200)
+
+image = cv2.imread(path)
+rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+text = pytesseract.image_to_string(rgb_image, lang='kor+eng')
+st.text(text)
+
+
+import ffmpeg
+import whisper
+
+model = whisper.load_model("base")
+from IPython.display import Audio
+Audio("C:/Users/wndud/Downloads/aaa.mp3")
+def transcribe(audio):
+    
+    # load audio and pad/trim it to fit 30 seconds
+    audio = whisper.load_audio(audio)
+    audio = whisper.pad_or_trim(audio)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    print(f"Detected language: {max(probs, key=probs.get)}")
+
+    # decode the audio
+    options = whisper.DecodingOptions(fp16=False)
+    result = whisper.decode(model, mel, options)
+    return result.text
+
+easy_text = transcribe("C:/Users/wndud/Downloads/aaa.mp3")
+st.text(easy_text)
+
+import bardapi
+import os
+os.environ['_BARD_API_KEY']='WwhjGcfngiK0STZR7o3fdNL3Kn25k1elab5Y-AuJKhDYpS64msPT_Tfsv6OyKppZsZv_mQ.'
+
+
+
+input_text = '나는 메밀 알레르기가 있어 이 음식을 먹어도 될까?' + text 
+li = []
+response = bardapi.core.Bard().get_answer(input_text)
+for i , choice in enumerate(response['choices']):
+##    li.append(f"Choice {i+1}:\n", choice['content'][0], "\n")
+    li.append(choice['content'][0])
+    
+st.markdown(li[0])
+
+input_text = easy_text
+li = []
+response = bardapi.core.Bard().get_answer(input_text)
+for i , choice in enumerate(response['choices']):
+##    li.append(f"Choice {i+1}:\n", choice['content'][0], "\n")
+    li.append(choice['content'][0])
+    
+st.markdown(li[0])
