416e199b5cbd6d37bedfdf73bc2291433b2c38a0
zoo00 <104506641+zoo00@users.noreply.github.com>
2023-06-13 16:07:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,23 +23,23 @@ import ffmpeg
 import whisper
 
 
-import speech_recognition as sr
-#import sys #-- 텍스트 저장시 사용
+# import speech_recognition as sr
+# #import sys #-- 텍스트 저장시 사용
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say Something")
-    speech = r.listen(source)
+# r = sr.Recognizer()
+# with sr.Microphone() as source:
+#     print("Say Something")
+#     speech = r.listen(source)
 
-#sys.stdout = open('audio_output.txt', 'w') #-- 텍스트 저장시 사용
+# #sys.stdout = open('audio_output.txt', 'w') #-- 텍스트 저장시 사용
 
-try:
-    audio = r.recognize_google(speech, language="ko-KR")
-    print("Your speech thinks like\n " + audio)
-except sr.UnknownValueError:
-    print("Your speech can not understand")
-except sr.RequestError as e:
-    print("Request Error!; {0}".format(e))
+# try:
+#     audio = r.recognize_google(speech, language="ko-KR")
+#     print("Your speech thinks like\n " + audio)
+# except sr.UnknownValueError:
+#     print("Your speech can not understand")
+# except sr.RequestError as e:
+#     print("Request Error!; {0}".format(e))
     
 # model = whisper.load_model("base")
 # from IPython.display import Audio
